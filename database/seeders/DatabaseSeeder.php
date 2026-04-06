037b3ec284758134cf2f9c781629f836f18fede9
Lukatsurtsumia <62548724+Lukatsurtsumia@users.noreply.github.com>
2026-04-06 22:48:31 +0200

    Refactor DatabaseSeeder to create users and books

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,53 +2,43 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Support\Facades\App;
-
 use App\Models\Book;
 use App\Models\User;
-
 use App\Models\Category;
-use Database\Seeders\CategorySeeder;
- 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
-{
-    // run fixed categories
-    $this->call(CategorySeeder::class);
+    {
+        // Create users FIRST
+        User::factory(10)->create();
 
-    $categories = Category::all();
+        // Create admin
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password'),
+            'is_admin' => true
+        ]);
 
-    Book::factory(20)
-        ->hasReviews(10)
-        ->create()
-        ->each(function ($book) use ($categories) {
-            $book->categories()->attach(
-                $categories
-                    ->random(rand(1,3))
-                    ->pluck('id')
-            );
+        // Run categories
+        $this->call(CategorySeeder::class);
 
-            $book->update([
-                'image' => 'books/' . rand(1,20) . '.jpg'
-            ]);
-        });
+        $categories = Category::all();
 
-    // create admin user
-    User::factory()->create([
-        'name' => 'Admin',
-        'email' => 'admin@gmail.com',
-        'password' => bcrypt('password'),
-        'is_admin' => true
-    ]);
-}
-   
+        // Create books
+        Book::factory(20)
+            ->hasReviews(10)
+            ->create()
+            ->each(function ($book) use ($categories) {
+                $book->categories()->attach(
+                    $categories->random(rand(1,3))->pluck('id')
+                );
+
+                $book->update([
+                    'image' => 'books/' . rand(1,20) . '.jpg'
+                ]);
+            });
+    }
 }
