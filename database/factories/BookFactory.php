5ebb2db1de91e2cc59447226afbe9e13301ef0d3
Lukatsurtsumia <62548724+Lukatsurtsumia@users.noreply.github.com>
2026-04-06 21:45:09 +0200

    Refactor BookFactory to use $this->faker

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Database\Factories;
 
 use App\Models\User;
@@ -9,15 +8,13 @@ class BookFactory extends Factory
 {
     public function definition(): array
     {
-        $faker = \Faker\Factory::create();
-
         return [
             'image' => null,
-            'title' => $faker->sentence(3),
-            'author' => $faker->name(),
-            'description' => $faker->paragraph(),
-            'year' => $faker->year(),
-            'price' => $faker->randomFloat(2, 5, 100),
+            'title' => $this->faker->sentence(3),
+            'author' => $this->faker->name(),
+            'description' => $this->faker->paragraph(),
+            'year' => $this->faker->year(),
+            'price' => $this->faker->randomFloat(2, 5, 100),
             'user_id' => User::factory(),
         ];
     }
