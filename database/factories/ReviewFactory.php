0214ef7e6a51d3fa64a3007470e99d975168fbcf
Lukatsurtsumia <62548724+Lukatsurtsumia@users.noreply.github.com>
2026-04-07 14:38:06 +0200

    Update ReviewFactory.php

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -5,20 +5,17 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
- */
 class ReviewFactory extends Factory
 {
     public function definition(): array
     {
-        $createdAt = fake()->dateTimeBetween('-10 years', 'now');
-        $updatedAt = fake()->dateTimeBetween($createdAt, 'now');
+        $createdAt = $this->faker->dateTimeBetween('-10 years', 'now');
+        $updatedAt = $this->faker->dateTimeBetween($createdAt, 'now');
 
         return [
             'user_id' => User::inRandomOrder()->first()->id,
-            'rating' => fake()->numberBetween(1, 5),
-            'comment' => fake()->paragraph(),
+            'rating' => $this->faker->numberBetween(1, 5),
+            'comment' => $this->faker->paragraph(),
             'created_at' => $createdAt,
             'updated_at' => $updatedAt,
         ];
