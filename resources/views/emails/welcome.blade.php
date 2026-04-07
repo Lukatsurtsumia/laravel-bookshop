41204b99208dad98eb0fc3efe67c52f1ff7bd642
Lukatsurtsumia <62548724+Lukatsurtsumia@users.noreply.github.com>
2026-04-08 00:38:07 +0200

    Update welcome.blade.php

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,16 +1,15 @@
-<div style="text-align:center; font-family: Arial, sans-serif;"> 
-@if(app()->environment('local'))
-    <img src="{{ $message->embed(public_path('storage/books/cover.jpg')) }}" width="150">
-@else
-    <img src="{{ asset('storage/books/cover.jpg') }}" width="200">
-@endif
+<div style="text-align:center; font-family: Arial, sans-serif;">
+
+<img src="{{ asset('storage/books/cover.jpg') }}" width="200">
+
 <h2>Welcome {{ $user->name }}!</h2>
 
 <p>Your account was created successfully!</p>
 
 <p>
-You received <strong>{{ $user->points }}</strong> bonus points.
+You received <strong>{{ $user->points ?? 0 }}</strong> bonus points.
 </p>
 
 <p>Enjoy our BookShop 📚</p>
- </div>
+
+</div>
